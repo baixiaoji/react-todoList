db52e89de5bf674f28dc872476ac1cefdd1c2f28
Kevin <1041874421@qq.com>
2017-04-01 10:18:47 +0800

    引用自带插件压缩，可是很慢

---
+++ app/foo.js
@@ -0,0 +1,3 @@
+export default function(){
+    return "学习webpack-2的教程"
+}
--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import _ from 'lodash'
 import $ from 'jquery'
+import foo from './foo'
+
 function component(){
   var element = $("<div></div>")
 
   element.html( _.join(["hello","baiji"]," "))
-
+  console.log(foo)
+  console.log(foo())
   return element[0]
 }
 
