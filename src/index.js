e5f71dcebb2ed980db4e4166f565d0a6b841800d
Kevin <1041874421@qq.com>
2017-04-04 13:59:04 +0800

    学习react 1.引用create-react-app 2.安装了yarn yarn-start 启动服务 3. 使用ReactDOM.render()

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <h1>Hi, world!</h1>,
+  document.getElementById('root2')
+);
