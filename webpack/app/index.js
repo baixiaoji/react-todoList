8a723c5019ee83521131eb7ff2c04beffbd6345f
Kevin <1041874421@qq.com>
2017-04-04 13:42:08 +0800

    import the webpack-dev-server

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -5,7 +5,7 @@ import foo from './foo'
 function component(){
   var element = $("<div></div>")
 
-  element.html( _.join(["Hi","baiji"]," "))
+  element.html( _.join(["Hello","baixiaoji"]," "))
   console.log(foo)
   console.log(foo())
   return element[0]
