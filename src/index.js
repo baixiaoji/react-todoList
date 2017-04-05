bc3c73b6615aeccb6386d416eba634df357f36ae
Kevin <1041874421@qq.com>
2017-04-05 17:40:59 +0800

    引用bulma 1.css使用@import放在CSS最后 2. JS引用import 只要前面没有./ ../则默认还是去node_modules里找 3.bulma的样式没有看完

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+//不要是. ..打头的引用都是去node_modules里找
+import 'bulma/css/bulma.css';
 
 ReactDOM.render(
-  <h1>Hi, world!</h1>,
-  document.getElementById('root2')
+  <div className="hero is-dark is-fullheight">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <h1 className="title">
+          Hello world
+        </h1>
+        <h2 className="subtitle">
+          learn-react
+        </h2>
+      </div>
+    </div>
+  </div>,
+  document.getElementById('root')
 );
