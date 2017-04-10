6fc4db352fd9ca2947186655019216748d219fc7
Kevin <1041874421@qq.com>
2017-04-10 19:26:43 +0800

    报错显示 1. this是null那么说明没有绑定this所以使用bind绑定this

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,7 +4,7 @@ export default class TodoInput extends Component{
     render(){
         return <input type="text" defaultValue="this.props.content" 
         
-        onKeyPress={this.submit}/>
+        onKeyPress={this.submit.bind(this)}/>
     }
 
     submit(e){
