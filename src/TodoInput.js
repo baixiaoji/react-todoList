9ecdd25ebf2da95051e88d260a566ad3dcd34d48
Kevin <1041874421@qq.com>
2017-04-10 19:14:44 +0800

    因为报错显示要将value更为defaultvalue

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,6 +2,6 @@ import React, {Component } from "react"
 
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" value="this.props.content"/>
+        return <input type="text" defaultValue="this.props.content"/>
     }
 }
