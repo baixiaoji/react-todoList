981c837f16bcec7bc6624c7d400283b4c83d7efc
Kevin <1041874421@qq.com>
2017-04-10 18:28:35 +0800

    lifecycle 1.记得看文档 2. 调 什么时候才能使用setState测试

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,77 @@
 import React from 'react';
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1  className="title">Hello,{this.props.name}</h1>;
-//   }
-// }
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props)
 
-function Welcome(props){
-  return <h1 className="title"> Hello,{props.name}</h1>
+    this.state = {
+      date: new Date(),
+      test: "1"
+    }
+
+    this.setState({
+      date: new Date(),
+      test: 'constructor'
+    })
+
+    console.log("我是在constructor里面将props和state初始化好了")
+  }
+  componentWillMount(){
+    this.setState({
+      date: new Date(),
+      test: "componentWillMount"
+    })
+
+    console.log("运行到这里，说明马上就运行render")
+  }
+  render() {
+   
+    console.log("这里就是 render")
+    return (
+      <div>
+        <h1 className="title">Hello,{this.props.name}</h1>
+        <h2> {this.state.date.toString()}</h2>
+        <p>{this.state.test}</p>
+      </div>
+    );
+  }
+  componentDidMount(){
+    this.setState({
+      date: new Date(),
+      test: "componentDidMount"
+    })
+    console.log("已经挂载到页面里了")
+  }
+
+  componentWillReceiveProps(){
+    this.setState({
+      date: new Date(),
+      test: "componentWillReceiveProps"
+    })
+  }
+
+  shouldComponentUpdate(){
+    this.setState({
+      date: new Date(),
+      test: "shouldComponentUpdate"
+    })
+
+    return true;
+  }
+
+  componentDidUpdate(){
+    this.setState({
+      date: new Date(),
+      test: "componentDidUpdate"
+    })
+  }
+
+  componentWillUnmount(){
+    console.log("结束了")
+  }
 }
+
+// function Welcome(props){
+//   return <h1 className="title"> Hello,{props.name}</h1>
+// }
 // module.exports = Welcome;
 export default Welcome
