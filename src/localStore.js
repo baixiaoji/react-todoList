da5924263b8cc0d2fde3d5d7c214591f0c9b8f22
Kevin <1041874421@qq.com>
2017-04-20 13:14:20 +0800

    yarn build the react-todoList-demo

--- src/localStore.js
+++
@@ -1,7 +0,0 @@
-export function save(key,value){
-    return window.localStorage.setItem(key,JSON.stringify(value))
-}
-
-export function load(key){
-    return JSON.parse(window.localStorage.getItem(key))
-}
